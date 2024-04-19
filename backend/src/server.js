95056023d3da5337282873cdcb6d3379ac75865a
IgorLima <igorlimadrum@gmail.com>
2024-04-19 10:08:38 -0300

    feat: initialize server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
+const app = require("./app.js");
 const postgresPool = require("./common/database/postgress.js");
+
+const port = process.env.API_PORT || "8081";
+
 console.log("Connecting to database...");
 postgresPool
   .connect()
   .then(() => {
+    app.listen(port, () =>
+      console.log("listening on port http://localhost:" + port)
+    );
   })
   .catch((err) => {
     console.log(

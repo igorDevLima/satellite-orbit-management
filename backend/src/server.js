24c335e2a125ce3186f25fb487657a2381dab774
IgorLima <igorlimadrum@gmail.com>
2024-04-26 15:26:09 -0300

    feat: add a server  with postgres database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,19 @@
 const app = require("./app.js");
-const postgresPool = require("./common/database/postgress.js");
+const { connect } = require("./common/database/postgress.js");
 
 const port = process.env.API_PORT || "8081";
 
-console.log("Connecting to database...");
-postgresPool
-  .connect()
-  .then(() => {
-    app.listen(port, () =>
-      console.log("listening on port http://localhost:" + port)
-    );
-  })
-  .catch((err) => {
-    console.log(
-      "Database connection error\n" + process.env.NODE_ENV === "development"
-        ? err
-        : ""
-    );
-  });
+async () =>
+  await connect()
+    .then(() => {
+      app.listen(port, () =>
+        console.log("listening on port http://localhost:" + port)
+      );
+    })
+    .catch((err) => {
+      console.log(
+        "Database connection error\n" + process.env.NODE_ENV === "development"
+          ? err
+          : ""
+      );
+    });
